88bffd931b15624e90164c801a3fb39d1c4fdd0d
DESKTOP-JPH4LMO\Aaronzm <zhangming_oo@icloud.com>
2018-08-18 14:56:18 +0800

    更新

--- utils/date/getDate.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-module.exports = function (dateStr) {
-    let dateArr = dateStr.split(/[- : \/]/);
-    return new Date(+dateArr[0], +dateArr[1] - 1, dateArr[2] || '01', dateArr[3] || '00', dateArr[4] || '00', dateArr[5] || '00');
-}
